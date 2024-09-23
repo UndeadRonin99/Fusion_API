b6e74a411b4952743f454287c2de04553098b7f8
UndeadRonin99 <ST10046014@vcconnect.edu.za>
2024-09-23 14:27:12 +0200

    changed code to use base 64 firebase service key

--- services/firebaseService.js
+++ services/firebaseService.js
@@ -1,14 +1,26 @@
-// services/firebaseService.js
-
 const admin = require('firebase-admin');
-const serviceAccount = require('../firebase/serviceAccountKey.json');
 
-// Initialize Firebase Admin SDK
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://fusion-14429-default-rtdb.firebaseio.com/',
-});
+// Check if running in a deployment environment
+if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY_BASE64) {
+  // Decode the base64-encoded service account key
+  const serviceAccountBuffer = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_BASE64, 'base64');
+  const serviceAccount = JSON.parse(serviceAccountBuffer.toString('utf-8'));
 
-const rtdb = admin.database();
+  // Initialize Firebase Admin SDK
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://your-firebase-project.firebaseio.com',
+  });
+} else {
+  // For local development, use the serviceAccountKey.json file
+  const serviceAccount = require('../firebase/serviceAccountKey.json');
 
-module.exports = { admin, rtdb };
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://your-firebase-project.firebaseio.com',
+  });
+}
+
+const db = admin.firestore();
+
+module.exports = { admin, db };
